6630f7a7be87ba08dc02a51a0ed8283befe06954
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2019-02-14 02:03:44 +0800

    Initial successful parsing of ALF files

--- melmac.py
+++ melmac.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import ply.lex as lex
+import sys
+import re
+
+from antlr4 import *
+from melmac_alf import MelmacALFData
+from alf_parser.grammar.ALFLexer import ALFLexer
+from alf_parser.grammar.ALFParser import ALFParser
+from alf_parser.grammar.ALFListener import ALFListener
 
 class Melmac(object):
-	def __init__(self):
-		print("Initializing Melmac")
+	def __init__(self, filename):
+		lexer = ALFLexer(FileStream(filename))
+		stream = CommonTokenStream(lexer)
+		parser = ALFParser(stream)
+		tree = parser.start()
+		data = MelmacALFData(tree)
 
-main = Melmac()
+
+if len(sys.argv)>1:
+	main = Melmac(sys.argv[1])
---
+++ melmac_alf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from antlr4 import *
+
+class MelmacALFData(object):
+	parsing_unit = False
+	unit_names = ['TIME','FREQUENCY','DISTANCE','AREA','VOLTAGE','CURRENT','ENERGY','POWER','CAPACITANCE','RESISTANCE','INDUCTANCE']
+	recent_unit_name = None
+
+	def __init__(self, expr):
+		self.handleExpression(expr)
+
+	def handleExpression(self, expr):
+		for child in expr.getChildren():
+			if isinstance(child, tree.Tree.TerminalNode):
+				token=child.getText()
+				print(token)
+
+				if token in self.unit_names:
+					self.recent_unit_name=token
+				if token in 'UNIT':
+					print("Found unit declaration "+self.recent_unit_name)
+
+				#print("Parsed expression %s" % (expr.getText()))
+			else:
+				self.handleExpression(child)
